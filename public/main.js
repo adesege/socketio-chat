297570e546d4f0b26a4aacea5f3b146e6d4722d2
Fadojutimi Temitayo Olusegun <Fadojutimitemitayo@gmail.com>
2018-03-27 11:36:39 +0100

    implement login modal

--- public/main.js
+++ public/main.js
@@ -1,5 +1,46 @@
 const socket = io();
 
-socket.on('CONNECTED', () => {
-  console.log('Client connected')
+const $modalContainer = document.getElementById('modal');
+const $usernameInput = document.getElementById('username-input');
+const $emailInput = document.getElementById('email-input');
+const $buttonOK = document.getElementById('button-ok');
+const $chatWrapper = document.getElementById('chat-wrapper');
+const $errorSpan = document.getElementsByClassName('error-message')[0];
+
+let isConnected = false;
+
+$modalContainer.showModal();
+
+const showErrorMessage = ({ message }) => {
+  $errorSpan.innerText = message;
+  $errorSpan.classList.remove('hidden');
+}
+
+const login = () => {
+  const username = $usernameInput.value.trim();
+  const email = $emailInput.value.trim();
+
+  if (username && email) {
+    socket.emit('LOGIN', { username, email });
+  }
+
+  socket.on('LOGIN_ERROR', (data) => {
+    showErrorMessage(data);
+  });
+
+  socket.on('LOGIN_SUCCESSFUL', () => {
+    isConnected = true;
+    $modalContainer.close();
+    $chatWrapper.classList.remove('hidden');
+  });
+};
+
+socket.on('connect', () => {
+  console.log('Client connected');
+});
+
+socket.on('USER_JOINED', (data) => {
+  if(isConnected) {
+    console.log('USER_JOINED', data);
+  }
 });
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // import required dependencies
 const express = require('express');
+const md5 = require('md5');
 const path = require('path');
 const http = require('http');
 const socketIO = require('socket.io');
@@ -13,9 +14,27 @@ app.use(express.static(path.join(__dirname, 'public')));
 const server = http.createServer(app);
 const io = socketIO(server);
 
+const users = {};
+
 io.on('connection', (socket) => {
   console.log('Server connected');
-  socket.emit('CONNECTED', () => { })
+  socket.on('LOGIN', (data) => {
+    if(Object.keys(users).indexOf(data.username) !== -1) {
+      socket.emit('LOGIN_ERROR', { message: 'Username already taken. Please choose a different username' });
+      return;
+    }
+
+    socket.emit('LOGIN_SUCCESSFUL');
+
+    const userObject = {
+      username: data.username,
+      imgUrl: `https://www.gravatar.com/avatar/${md5(data.email)}?d=identicon`
+    };
+
+    users[data.username] = userObject;
+
+    socket.broadcast.emit('USER_JOINED', userObject);
+  });
 });
 
 server.listen(port, () => {
